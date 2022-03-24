699b8e31ddcdf3f676ee2ad8fcc7ab7ce81b7046
ravirasadiya <ravirasadiya1994@gmail.com>
2022-03-24 16:20:33 +0530

    corrections

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -7,10 +7,10 @@ export default function Footer() {
       <Box as='footer' className='footer'>
         <Heading as="h4">Social Media</Heading>
         <Box className='social_lin_prnt'>
-          <Button as="a" href='#' data-aos="fade-up" data-aos-delay="400"><Image src='img/social_ic01.svg' alt='' /></Button>
-          <Button as="a" href='#' data-aos="fade-up" data-aos-delay="500"><Image src='img/social_ic02.svg' alt='' /></Button>
-          <Button as="a" href='#' data-aos="fade-up" data-aos-delay="600"><Image src='img/social_ic03.svg' alt='' /></Button>
-          <Button as="a" href='#' data-aos="fade-up" data-aos-delay="700"><Image src='img/social_ic04.svg' alt='' /></Button>
+          <Button as="a" href='#' data-aos="fade-down" data-aos-delay="400"><Image src='img/social_ic01.svg' alt='' /></Button>
+          <Button as="a" href='#' data-aos="fade-down" data-aos-delay="500"><Image src='img/social_ic02.svg' alt='' /></Button>
+          <Button as="a" href='#' data-aos="fade-down" data-aos-delay="600"><Image src='img/social_ic03.svg' alt='' /></Button>
+          <Button as="a" href='#' data-aos="fade-down" data-aos-delay="700"><Image src='img/social_ic04.svg' alt='' /></Button>
         </Box>
         <Text>2022, All Rights Reserved.</Text>
       </Box>
